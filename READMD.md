429fdca4f99865237f8ce3bf1fe731b797f5bd20
jopemachine <jopemachine@naver.com>
2021-07-13 11:51:49 +0900

    init

---
+++ READMD.md
@@ -0,0 +1,17 @@
+# arvis-calculator-plugin
+
+Calculator plugin example for Arvis.
+
+![](./media/1.png)
+
+![](./media/2.png)
+
+![](./media/3.png)
+
+![](./media/4.png)
+
+## Related
+
+- [arvish](https://github.com/jopemachine/arvish) - Arvis workflow, plugin creator tools
+
+- [available plugins](https://github.com/jopemachine/arvis/blob/master/documents/plugin-links.md)
---
+++ index.js
@@ -0,0 +1,27 @@
+const path = require("path");
+const math = require("mathjs");
+
+const getPluginItem = ({ inputStr }) => {
+  let items = [];
+  try {
+    const result = String(math.evaluate(inputStr));
+
+    items.push({
+      title: result,
+      subtitle: inputStr != result ? `${inputStr} = ${result}` : inputStr,
+      icon: {
+        path: `${__dirname}${path.sep}icon.png`,
+      },
+    });
+  } catch (err) {
+    // If inputStr is not proper numerical expression, catchs err.
+    // Return an empty array to 'items' if you do not need to include this plugin's items.
+  }
+
+  return {
+    items,
+  };
+};
+
+// Export a function that has inputStr as a argument.
+module.exports = getPluginItem;
